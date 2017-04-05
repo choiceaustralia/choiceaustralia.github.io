f05bf2f56cf97ee3ad1172adc052ad0ffe1c901d
Anderson, Stuart (Data61, Acton) <stuart.anderson@csiro.au>
2017-04-05 22:18:39 +0800

    Move canvas to graphics container

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,41 @@
-let renderer = new THREE.WebGLRenderer({ antialias: true });
-let loader = new THREE.ObjectLoader();
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+window.onload = () => {
 
-camera.position.set(5.1, 0, 15);
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio || 1);
-document.body.appendChild(renderer.domElement);
+    let container = document.getElementById('graphics');
 
-let earth, sun, co2, sunlight;
+    let renderer = new THREE.WebGLRenderer({ antialias: true });
+    let loader = new THREE.ObjectLoader();
+    let scene = new THREE.Scene();
+    let camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 1, 1000);
 
-function update () {
-    window.requestAnimationFrame(update);
-    earth.rotation.y += 0.005;
-    renderer.render(scene, camera);
+    camera.position.set(5.1, 0, 15);
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio || 1);
+    container.appendChild(renderer.domElement);
+
+    let earth, sun, co2, sunlight;
+
+    function update () {
+        window.requestAnimationFrame(update);
+        earth.rotation.y += 0.005;
+        sun.rotation.y -= 0.002;
+        renderer.render(scene, camera);
+    }
+
+    function onResize () {
+        renderer.setSize(container.clientWidth, container.clientHeight);
+        camera.aspect = container.clientWidth / container.clientHeight;
+        camera.updateProjectionMatrix();
+    }
+
+    function init (obj) {
+        scene.add(obj);
+        earth = scene.getObjectByName('Earth');
+        sun = scene.getObjectByName('Sun');
+        co2 = scene.getObjectByName('CO2');
+        sunlight = scene.getObjectByName('Sunlight');
+        update();
+    }
+
+    window.addEventListener('resize', onResize);
+    loader.load('assets/scene.json', obj => init(obj));
 }
-
-function init (obj) {
-    scene.add(obj);
-    earth = scene.getObjectByName('Earth');
-    sun = scene.getObjectByName('Sun');
-    co2 = scene.getObjectByName('CO2');
-    sunlight = scene.getObjectByName('Sunlight');
-    update();
-}
-
-loader.load('assets/scene.json', obj => init(obj));
